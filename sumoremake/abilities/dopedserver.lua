36846cf110fa6abb95c0ee57ae7ca2c37c33df10
Glow <leovonandion@gmail.com>
2023-03-03 23:49:32 +0100

    replaced __resource.lua as depractedHAS BROKE CODE

--- sumoremake/abilities/dopedserver.lua
+++ sumoremake/abilities/dopedserver.lua
@@ -1,5 +1,4 @@
 RegisterNetEvent("dopeAllPlayers", function()
     local src = source
-    -- Run some checks on src, just make sure they are in the right condition to actually "dope all players"
     TriggerClientEvent("dopePlayer", -1)
 end)
---
+++ sumoremake/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+description 'Sumo Remake'
+ 
+client_scripts {
+    "main.lua",
+    "functions/zone.lua",
+    "functions/spawn.lua",
+    "functions/voicelines.lua",
+    "abilities/doped.lua",
+    "abilities/spawn.lua"
+} 
+ 
+server_script 'abilities/dopedserver.lua'
