481662c1810fc2d43dc1525d34719206b0c98f47
Glow <leovonandion@gmail.com>
2023-03-05 02:02:45 +0100

    powerup should spawn

--- sumoremake/abilities/spawn.lua
+++ sumoremake/abilities/spawn.lua
@@ -7,6 +7,12 @@ AddEventHandler("sumo:abil", function()
     SetBlipColour(blip1, 2)
     SetBlipRoute(blip1, false)
 
+    pickup2 = CreatePickupRotate(GetHashKey("PICKUP_PORTABLE_CRATE_FIXED_INCAR"), 219.48, 7905.74, 18.00, 180.1, 180.1, 210.1)
+    local blip2 = AddBlipForCoord(219.48, 7405.74, 17.46)
+    SetBlipSprite(blip2, 469)
+    SetBlipColour(blip2, 2)
+    SetBlipRoute(blip2, false)
+
 
 
     while true do
@@ -16,6 +22,15 @@ AddEventHandler("sumo:abil", function()
 
             RemoveBlip(blip1)
             dopeAllPlayers() 
+        end
+        Citizen.Wait(0)
+
+        if HasPickupBeenCollected(pickup2) then
+
+
+
+            RemoveBlip(blip2)
+            zoneAllPlayers() 
         end
         Citizen.Wait(0)
     end
